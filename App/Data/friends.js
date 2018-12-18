56257645660f1175a9a8dfcf7975928d61ed2c03
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2018-12-17 17:11:46 -0800

    Starting...

--- App/Data/friends.js
+++ App/Data/friends.js
@@ -1,2 +1,24 @@
 //  5. You should save your application's data inside of `app/data/friends.js` as an array of objects. 
 //     Each of these objects should roughly follow the format below.
+
+// This file holds the database of people as an array of objects as follows...
+
+// json
+// {
+//   "name":"Ahmed",
+//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "scores":[
+//       5,
+//       1,
+//       4,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       1
+//     ]
+// }
+
+
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// I think this is the file where everything runs off of
+
 var express = require("express");
 var path = require("path");
 
