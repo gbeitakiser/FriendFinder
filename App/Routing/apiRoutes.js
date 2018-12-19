e3161b533dea237da33a854b0c8032aef80a1d3d
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2018-12-18 18:28:21 -0800

    Ready for logic. HTML could stand to look prettier.

--- App/Routing/apiRoutes.js
+++ App/Routing/apiRoutes.js
@@ -1,22 +1,15 @@
-// 4. Your `apiRoutes.js` file should contain two routes:
 
-//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
-//      This route will also be used to handle the compatibility logic.
-
-var friends = require("../Data/friends");
+var respondants = require("../Data/friends");
 
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-        res.json(friends);
+        res.json(respondants);
       });
 
 
       app.post("/api/friends", function(req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body parsing middleware
+        
         respondants.push(req.body);
           res.json(true); // Will this be true or false?
           // Then add function for compatability logic
