87e3a5263ad483a3aa053cf931ea98274d9a5b45
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2018-12-19 18:58:56 -0800

    Logic working, make it return user matched with. Add CSS to make HTML look pretty, make code look pretty

--- App/Data/friends.js
+++ App/Data/friends.js
@@ -2,24 +2,39 @@
 // Main Database
 //================================
 
-var respondants = [
+var respondents = [
     {
         "name":"Spongebob Squarepants",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://pbs.twimg.com/profile_images/1002272769352978433/9S4QWSR0_400x400.jpg",
         "scores":[
-            4,
-            3,
-            2,
-            5,
-            3,
-            4,
-            2,
-            1,
-            4,
-            5
+            "4",
+            "3",
+            // 2,
+            // 5,
+            // 3,
+            // 4,
+            // 2,
+            // 1,
+            // 4,
+            // 5
+        ]
+    }, {
+        "name":"Sandy Squirrel",
+        "photo":"https://cdn-images-1.medium.com/max/1200/1*UybaRS4lAhgxVFDYuC_K0g.jpeg",
+        "scores":[
+            "5",
+            "4",
+            // 3,
+            // 4,
+            // 2,
+            // 1,
+            // 5,
+            // 3,
+            // 4,
+            // 1
         ]
     }
 ];
 
 
-module.exports = respondants;
+module.exports = respondents;
--- App/Routing/apiRoutes.js
+++ App/Routing/apiRoutes.js
@@ -1,17 +1,98 @@
+// Variables
+//===============================
+var respondents = require("../Data/friends");
+var newUserArr = [];
 
-var respondants = require("../Data/friends");
-
+// Holds Cross-File Communication
+//===============================
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-        res.json(respondants);
+        res.json(respondents);
       });
 
-
+      //
       app.post("/api/friends", function(req, res) {
+
+        // Gets new user's incoming scores and pushes them to array
+        //_________________________________________________________
+        for (var i = 0; i < req.body.scores.length; i++) {
+          newUserArr.push(+req.body.scores[i]);
+        };
+
+
+        // Compatability Checker
+        //===============================
+        // function matchUsers() {
+          var dbCompatabilityScore = [];
         
-        respondants.push(req.body);
-          res.json(true); // Will this be true or false?
-          // Then add function for compatability logic
+          // Loops through each person in database and adds their score to empty var. 
+          // Then initializes loops for each person that....(see next comment)
+          for (var i = 0; i < respondents.length; i++) {
+            var checkerSum = [];
+            var sum = 0;
+            var scoresToCheck;
+            var numbersToCheck = [];
+          
+            scoresToCheck = respondents[i].scores;
+        
+            // Converts person's scores from strings to integers and places them in new array. Then...
+            for (var v = 0; v < scoresToCheck.length; v++) {
+              numbersToCheck.push(parseInt(scoresToCheck[v]));
+            }
+        
+            // Compares each number in new user score to each number in score of selected user
+            // Number in x < (number) must reflect number of questions OR ELSE!!!
+            for (var x = 0; x < 2; x++) {
+              var newNumber;
+              newNumber = Math.abs(numbersToCheck[x] - newUserArr[x]);
+              checkerSum.push(newNumber);
+            }
+
+            // Sums everything in var checkerSum and gives compatability score. The lower it is, the more compatable
+            for (var c = 0; c < checkerSum.length; c++) {
+              sum += checkerSum[i];
+            }
+            dbCompatabilityScore.push(sum);           
+          }
+
+          indexOfSmallest(dbCompatabilityScore);
+          // Checks lowest number in dbCompatabilityScore Array and returns its index
+          function indexOfSmallest(a) {
+            var lowest = 0;
+            for (var i = 1; i < a.length; i++) {
+              if (a[i] < a[lowest]) lowest = i;
+            }
+            console.log("\n index of? lowest number:")
+            console.log(lowest) ;
+          }
+          console.log("\nCompatability Scores: ")
+          console.log(dbCompatabilityScore);
+          
+        // }
+
+
+
+        // Adds respondent data to respondents array in friends.js
+        respondents.push(req.body);
+          res.json(true); // Will this be true or false? And why do I need it?
       });
 };
+
+
+
+// 6. Determine the user's most compatible friend using the following as a guide:
+
+//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+//      * Example:
+//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+//        * Total Difference: **2 + 1 + 2 =** **_5_**
+//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+//    * The closest match will be the user with the least amount of difference.
+
+// 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+//    * The modal should display both the name and picture of the closest match.
+
+
